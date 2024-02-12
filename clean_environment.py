cad9a743d389bb1d47f06a8d2a44e93837fd1c26
ofirzvishaboo <ofirfirst@gmail.com>
2024-02-12 18:26:19 +0200

    removing uneeded stop server for frontend

--- clean_environment.py
+++ clean_environment.py
@@ -6,8 +6,8 @@ try:
 except requests.exceptions.RequestException as e:
     print(f"Error stopping server at 127.0.0.1:5000: {e}")
 
-try:
-    response = requests.get('http://127.0.0.1:5001/stop_server')
-    response.raise_for_status()
-except requests.exceptions.RequestException as e:
-    print(f"Error stopping server at 127.0.0.1:5001: {e}")
+# try:
+#     response = requests.get('http://127.0.0.1:5001/stop_server')
+#     response.raise_for_status()
+# except requests.exceptions.RequestException as e:
+#     print(f"Error stopping server at 127.0.0.1:5001: {e}")
