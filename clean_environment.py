95e5aa58f1ef31ba820e03304cbf1225730cb560
ofirzvishaboo <ofirfirst@gmail.com>
2024-02-12 21:49:05 +0200

    push me

--- clean_environment.py
+++ clean_environment.py
@@ -4,7 +4,7 @@ try:
     response = requests.get('http://127.0.0.1:5000/stop_server')
     response.raise_for_status()  # Raise an exception for non-200 status codes
 except requests.exceptions.RequestException as e:
-    print(f"Error stopping server at 127.0.0.1:5000: {e}")
+    print(f"Error stopping server at 127.0.0.1:3000: {e}")
 
 # try:
 #     response = requests.get('http://127.0.0.1:5001/stop_server')
