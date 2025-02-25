326d0a60f9ed51a65beb6026666100a467d22d1d
ofirzvishaboo <134959918+ofirzvishaboo@users.noreply.github.com>
2025-02-25 14:51:56 +0200

    Update db_connector.py

--- db_connector.py
+++ db_connector.py
@@ -5,7 +5,7 @@ import pymysql
 
 class DBConnector:
     # change host to localhost for jenkinsfile-1
-    def __init__(self, host="agent", port=3306, user='root', passwd='pythoncourse', db='my_db'):
+    def __init__(self, host="host.docker.internal", port=3306, user='root', passwd='pythoncourse', db='my_db'):
         self.db = db
         self.host = os.environ.get("database_host") or host
         self.port = port
