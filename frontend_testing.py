695ef794f876fc7e5552872e19b9b548769e5698
ofirzvishaboo <134959918+ofirzvishaboo@users.noreply.github.com>
2025-02-27 19:44:35 +0200

    Update frontend_testing.py

--- frontend_testing.py
+++ frontend_testing.py
@@ -2,12 +2,20 @@ import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+# Use the Selenium Grid URL instead of a local driver path
 
 
 class FrontEndTests:
     def __init__(self, user_id=1):
         driver_path = os.environ.get('DRIVER_PATH')
-        self.driver = webdriver.Chrome(service=Service(driver_path))
+        self.driver = webdriver.Remote(
+        command_executor='http://localhost:4444/wd/hub',
+        desired_capabilities=DesiredCapabilities.CHROME
+        )
+        # self.driver = webdriver.Chrome(service=Service(driver_path))
         self.driver.implicitly_wait(10)
         self.driver.get(f'http://127.0.0.1:5001/users/get_user_data/{user_id}')
 
