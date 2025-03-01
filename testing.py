04eb483ccd685b9fb704f3cc7dcdb8515f43f3bd
ofirzvishaboo <ofirfirst@gmail.com>
2025-03-01 18:39:53 +0200

    fixing testing

--- testing.py
+++ testing.py
@@ -1,4 +1,4 @@
 import requests
 
-res = requests.post("http://0.0.0.0:3000/users/5", json={'user_name': 'ofir'})
+res = requests.post("http://devops-rest:3000/users/5", json={'user_name': 'ofir'})
 print(res.status_code)
