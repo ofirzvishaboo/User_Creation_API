945a2c721c86da0c8ecba47ddcf28d426242f31a
ofirzvishaboo <ofirfirst@gmail.com>
2025-03-01 18:28:16 +0200

    fixing to add user

--- testing.py
+++ testing.py
@@ -1,4 +1,4 @@
 import requests
 
-res = requests.get("http://0.0.0.0:3000/users/1")
-print(res.json())
+res = requests.post("http://0.0.0.0:3000/users/5", json={'user_name': 'ofir'})
+print(res.status_code)
